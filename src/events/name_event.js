fa7dfe43c38d775b3e5df5c001bdb223ab199b59
Reicher <robin.o.reicher@gmail.com>
2017-05-24 00:01:42 +0200

    working name_event!
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/events/name_event.js
+++ src/events/name_event.js
@@ -1,30 +1,42 @@
 import Phaser from 'phaser'
 import StoryEvent from './story_event'
 import PhaserInputPackage  from 'phaser-input'
-const {PhaserInput} = PhaserInputPackage;  
+
+const {PhaserInput} = PhaserInputPackage;
+
 export default class NameEvent extends StoryEvent {
-	constructor(game) {
-		super(game)
-	}
+    constructor(game) {
+	super(game)
+    }
 
-	start() {
-		super.start()
-		console.log("NameEvent - start")
-		var password = game.add.inputField(10, 90, {
-			font: '18px Arial',
-			fill: '#212121', 
-			fontWeight: 'bold',
-			width: 150,
-			padding: 8,
-			borderWidth: 1,
-			borderColor: '#000',
-			borderRadius: 6,
-			placeHolder: "BULTEN",
-			type: PhaserInput.InputType.password
-		});
-	
-		this.game.player.name = "Anders"
+    accept(){
+	this.inputBox.endFocus();
+	this.game.player.name = this.inputBox.value;
+	this.inputBox.destroy()
+	this.end();
+    }
 
-		this.game.input.onDown.addOnce(this.end, this)
-	}
+    start() {
+	super.start()
+	console.log("NameEvent - start")
+	this.inputBox = game.add.inputField(this.game.world.centerX, game.world.centerY, {
+	    font: '30px Arial',
+	    fill: '#212121',
+	    fontWeight: 'bold',
+	    width: 200,
+	    padding: 8,
+	    borderWidth: 1,
+	    borderColor: '#000',
+	    borderRadius: 6,
+	    placeHolder: "KissOchBajs",
+	    type: PhaserInput.InputType.text
+	});
+	this.inputBox.x -= this.inputBox.width/2;
+        this.inputBox.blockInput = false;
+        this.inputBox.startFocus();
+
+        this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER)
+	    .onDown.addOnce(this.accept, this);
+
+    }
 }
